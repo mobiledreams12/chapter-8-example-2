25f498b0944e19ed9611d65fc36294a44970d792
Michael Taylor <mobiledreams12@yahoo.co.uk>
2026-01-22 16:17:54 +0000

    Fix broken test

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -6,6 +6,6 @@ describe("video streaming microservice", () => {
     test("microservice can handle requests", async () => {
 
         const response = await request(app).get("/live"); // Makes a request to the "/live" route.
-        expect(response.status).toBe(500); // Verify that a HTTP status code 200 is returned, indicating success.
+        expect(response.status).toBe(200); // Verify that a HTTP status code 200 is returned, indicating success.
     });
 });
